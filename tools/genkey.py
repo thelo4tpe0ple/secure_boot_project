2a153652f1b285ffd48ab06fd75005329e8b87ec
zch <zch@DESKTOP-4ST61IE>
2026-07-11 17:42:49 +0800

    feat:新增公私钥对生成脚本

---
+++ tools/genkey.py
@@ -0,0 +1,28 @@
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import serialization
+
+private_key = rsa.generate_private_key(
+    public_exponent=65537,
+    key_size=2048
+)
+
+public_key = private_key.public_key()
+
+# 保存私钥
+with open("private.pem", "wb") as f:
+    f.write(
+        private_key.private_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PrivateFormat.PKCS8,
+            encryption_algorithm=serialization.NoEncryption()
+        )
+    )
+
+# 保存公钥
+with open("public.pem", "wb") as f:
+    f.write(
+        public_key.public_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PublicFormat.SubjectPublicKeyInfo
+        )
+    )
